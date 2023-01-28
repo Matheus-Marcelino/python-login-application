4ee033eda797c42b7c7ed614e60df0575df2f759
Matheus Marcelino <110202966+Matheus-Marcelino@users.noreply.github.com>
2023-01-28 11:56:56 -0300

    Dando inicio

---
+++ main.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.uix import button
+from kivy.uix import label
+from kivy.uix import layout
+from kivy.uix import checkbox
+from kivy.uix import image
+from kivy.uix import textinput
+
+
+class Aplication(App):
+    def build(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = Aplication()
+    app.run()
