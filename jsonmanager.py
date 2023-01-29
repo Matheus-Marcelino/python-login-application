3e1f2b3ebdb9a1d7139b8369d2ff4c9b26d13011
Matheus Marcelino <110202966+Matheus-Marcelino@users.noreply.github.com>
2023-01-28 21:37:51 -0300

    Hash md5 configurado

--- jsonmanager.py
+++ jsonmanager.py
@@ -1,15 +1,16 @@
 from json import load, dump
 from json.decoder import JSONDecodeError
+from hashlib import md5
 
 
 class JsonManager:
     def __init__(self) -> None:
         self.__FILE: str = 'data.json'    
 
-    def load_file(self):
+    def load_file(self) -> dict:
         try:
             with open(self.__FILE, encoding='utf-8') as f:
-                obj_json = load(f)
+                obj_json: dict = load(f)
         except (FileNotFoundError, JSONDecodeError):
             with open(self.__FILE, 'w+',encoding='utf-8') as file_error_json:
                 SHOW_ERROR: dict = {'message': 'error'}
@@ -20,5 +21,10 @@ class JsonManager:
         return obj_json
 
     def insert(self, data: dict) -> None:
+        ENCODED: tuple[bytes, bytes] = (f"{data['user']}".encode('utf-8'),
+                                        f"{data['pass']}".encode('utf-8'))
+        data['user'] = md5(ENCODED[0]).hexdigest()
+        data['pass'] = md5(ENCODED[1]).hexdigest()
+
         with open(self.__FILE, 'w+', encoding='utf-8') as f:
-            dump(data, f, indent=4)
+           dump(data, f, indent=4)
