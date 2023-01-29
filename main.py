46141247f711e78d3bb7002b1cc2f99f074058a8
Matheus Marcelino <110202966+Matheus-Marcelino@users.noreply.github.com>
2023-01-29 14:19:45 -0300

    primeira parte do front-end finalizado

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from kivymd.app import MDApp
+from kivymd.app import Builder
 from kivymd.app import FpsMonitoring
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.button import MDRectangleFlatButton
+from kivymd.uix.floatlayout import FloatLayout
+from kivymd.uix.label import MDIcon
 
 
 class Login(MDApp):
     FpsMonitoring().fps_monitor_start()
-    
+
+    def build(self):
+        return Builder.load_file('properties.kv')
 
 
 if __name__ == '__main__':
-    app = Login()
-    app.run()
+    Login().run()
