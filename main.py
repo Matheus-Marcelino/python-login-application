d2029940ba16a347b0fd3a7d1e7e21b8c77beab5
Matheus Marcelino <110202966+Matheus-Marcelino@users.noreply.github.com>
2023-01-29 23:14:28 -0300

    Segunda parte do front-end finalizada

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from kivymd.app import MDApp
 from kivymd.app import Builder
 from kivymd.app import FpsMonitoring
-from kivymd.uix.textfield import MDTextField
-from kivymd.uix.button import MDRectangleFlatButton
-from kivymd.uix.floatlayout import FloatLayout
-from kivymd.uix.label import MDIcon
+from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen
 
 
-class Login(MDApp):
+class Cadastro(MDApp, Screen):
     def build(self):
         FpsMonitoring().fps_monitor_start()
-        return Builder.load_file('properties.kv')
+        self.theme_cls.theme_style = 'Dark'
+        Window.minimum_height = 600
+        Window.minimum_width = 500
+        self.__FILE = Builder.load_file('properties_login.kv')
+        return self.__FILE
+
+    def on_start(self):
+        self.theme_cls.theme_style = 'Dark'
+
+    def change_theme(self):
+        if self.theme_cls.theme_style == 'Light':
+            self.theme_cls.theme_style = 'Dark'
+            self.__FILE.ids.fg_pass.color = '#2096f2'
+        else:
+            self.theme_cls.theme_style = 'Light'
+            Window.clearcolor = (234,234,234,1)
+            self.__FILE.ids.fg_pass.color = '#2096f2'
 
 
 if __name__ == '__main__':
-    Login().run()
+    Cadastro().run()
